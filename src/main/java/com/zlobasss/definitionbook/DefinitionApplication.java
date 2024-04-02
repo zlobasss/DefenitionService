9cf5625c603c718478ef3927da903441b2766760
zlobasss <115967505+zlobasss@users.noreply.github.com>
2024-04-02 11:42:23 +0700

    Added mvc config and main controller, rename defenition(book) application

--- src/main/java/com/zlobasss/definitionbook/DefinitionApplication.java
+++ src/main/java/com/zlobasss/definitionbook/DefinitionApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DefinitionbookApplication {
+public class DefinitionApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DefinitionbookApplication.class, args);
+        SpringApplication.run(DefinitionApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/zlobasss/definitionbook/config/MvcConfig.java
@@ -0,0 +1,30 @@
+package com.zlobasss.definitionbook.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("index.html").setViewName("index");
+    }
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/**")) {
+            registry.addResourceHandler("/**").addResourceLocations(
+                    CLASSPATH_RESOURCE_LOCATIONS);
+        }
+    }
+}
