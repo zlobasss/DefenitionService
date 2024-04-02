766380e02bbe1ad594624ae406ca3020bf7e88aa
zlobasss <115967505+zlobasss@users.noreply.github.com>
2024-04-02 11:07:01 +0700

    Init commit

---
+++ src/main/java/com/zlobasss/definitionbook/DefinitionbookApplication.java
@@ -0,0 +1,13 @@
+package com.zlobasss.definitionbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefinitionbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefinitionbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zlobasss/definitionbook/DefinitionbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlobasss.definitionbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefinitionbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
